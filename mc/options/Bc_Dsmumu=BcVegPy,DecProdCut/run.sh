4cee52f78c01ea204e42e5188566546387a08867
Igor Babuschkin <igor@babuschk.in>
2014-10-30 18:25:41 +0100

    Add Gauss options for Bd->D0mumu

--- mc/options/Bc_Dsmumu=BcVegPy,DecProdCut/run.sh
+++ mc/options/Bc_Dsmumu=BcVegPy,DecProdCut/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-SetupProject Gauss v45r5
 gaudirun.py options.py $GAUSSOPTS/Gauss-2012.py $GAUSSOPTS/GenStandAlone.py $DECFILESROOT/options/14175001.py $LBBCVEGPYROOT/options/BcVegPy.py
 
---
+++ mc/options/Bd_D0mumu=DecProdCut/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gaudirun.py options.py $GAUSSOPTS/Gauss-2012.py $GAUSSOPTS/GenStandAlone.py $DECFILESROOT/options/11174011.py $LBPYTHIAROOT/options/Pythia.py
+

76b6ae177ba2621a88d31f6c7d58ece2a0c72d7e
Igor Babuschkin <igor@babuschk.in>
2014-11-21 00:22:43 +0100

    Add more variables and explore classification possibilities

--- pipeline.py
+++ pipeline.py
@@ -16,6 +16,11 @@ Analysis pipeline:
 
 """
 
+import logging
+from log import setup_logging; setup_logging()
+
+from matplotlib.mlab import rec_append_fields as append_fields
+
 # Simulated data fetched from the grid
 input_mc = [
     './storage/MC/2012/Stripping20/AllStreams/DVBd2MuMuD0_MC/combined/Bd2MuMuD0.root'
@@ -29,6 +34,7 @@ input_data = [
 # Variables that are used in the analysis
 variables = [
         'B_M',
+        'B_DIRA_OWNPV',
         'B_FD_OWNPV',
         'B_ENDVERTEX_CHI2',
         'B_P',
@@ -41,6 +47,8 @@ variables = [
         'Psi_FDCHI2_ORIVX',
         'D~0_M',
         'D~0_FD_ORIVX',
+        'D~0_CosTheta',
+        'D~0_DIRA_OWNPV',
         'muplus_isMuon',
         'muplus_ProbNNpi',
         'muplus_ProbNNmu',
@@ -55,9 +63,11 @@ variables = [
         'Kplus_PX',
         'Kplus_PY',
         'Kplus_PZ',
+        'Kplus_PT',
         'piminus_PX',
         'piminus_PY',
         'piminus_PZ',
+        'piminus_PT',
         'Psi_PX',
         'Psi_PY',
         'Psi_PZ',
@@ -67,31 +77,37 @@ variables = [
         'piminus_ProbNNk',
         'piminus_ProbNNmu',
         'piminus_ProbNNp',
+        'B_L0MuonDecision_TOS',
+        'B_Hlt1TrackAllL0Decision_TOS',
+        'B_Hlt1TrackMuonDecision_TOS',
+        'B_Hlt2Topo2BodyBBDTDecision_TOS',
+        'B_Hlt2Topo3BodyBBDTDecision_TOS',
+        'B_Hlt2Topo4BodyBBDTDecision_TOS',
+        'B_Hlt2TopoMu2BodyBBDTDecision_TOS',
+        'B_Hlt2TopoMu3BodyBBDTDecision_TOS',
+        'B_Hlt2TopoMu4BodyBBDTDecision_TOS',
+        'Psi_Hlt2SingleMuonDecision_TOS',
+        'B_Hlt2DiMuonDetachedDecision_TOS',
 ]
 
 bdt_variables = [
-        'B_OWNPV_CHI2',
-        'B_ENDVERTEX_CHI2',
-        'B_P',
-        'B_PT',
         'B_ISOLATION_BDT_Soft',
-        'B_ISOLATION_BDT_Hard',
-        'Psi_FD_ORIVX',
-        'Psi_FDCHI2_ORIVX',
-        'piminus_ProbNNpi',
-        'piminus_ProbNNk',
-        'piminus_ProbNNmu',
+        'B_ENDVERTEX_CHI2',
+        'B_DIRA_OWNPV',
+        'D~0_DIRA_OWNPV',
+        'muplus_ProbNNmu',
+        'muminus_ProbNNmu',
         'Kplus_ProbNNpi',
         'Kplus_ProbNNk',
-        'Kplus_ProbNNmu',
-        'muminus_ProbNNpi',
-        'muminus_ProbNNmu',
-        'muplus_ProbNNpi',
-        'muplus_ProbNNmu',
+        'Kplus_ProbNNp',
+        'piminus_ProbNNpi',
+        'piminus_ProbNNp',
+        'Psi_FD_ORIVX',
 ]
 
 selection = [
-        #'Psi_M < 2850 || Psi_M > 3200',
+        'Psi_M < 2860 || Psi_M > 3200',
+        'Psi_M < 3500',
 ]
 
 mc_variables = [
@@ -115,12 +131,16 @@ from ruffus import *
 def reduce(infile, outfile):
     from root_numpy import root2array, array2root
     arr = root2array(infile, 'B2XMuMu_Line_TupleDST/DecayTree', variables)
+
+    arr = append_fields(arr, 'B_TAU', calc_tau(arr))
+    
     array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
 
 @transform(input_mc, suffix('.root'), '.reduced.root')
 def reduce_mc(infile, outfile):
     from root_numpy import root2array, array2root
-    arr = root2array(infile, 'B2XMuMu_Line_TupleMC/DecayTree', variables + mc_variables)
+    arr = root2array(infile, 'B2XMuMu_Line_TupleMC/DecayTree', variables + mc_variables, selection=prepare_sel(mc_selection))
+    arr = append_fields(arr, 'B_TAU', calc_tau(arr))
     array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
 
 @transform(reduce, suffix('.root'), '.blinded.root')
@@ -129,33 +149,36 @@ def blind_signalpeak(infile, outfile):
     B_mass = 5279
     width = 50
 
-    selstr = '(B_M < {}) | (B_M > {})'.format(B_mass - width, B_mass + width)
+    selstr = '(B_M < {}) || (B_M > {})'.format(B_mass - width, B_mass + width)
     arr = root2array(infile, 'B2dD0MuMu', selection=selstr)
-    print('Events in blinded dataset: ' + str(len(arr)))
-    array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
-
-@transform(reduce_mc, suffix('.root'), '.mccut.root')
-def select_mc(infile, outfile):
-    from root_numpy import root2array, array2root
-
-    selstr = ' & '.join(map(lambda x: ' ( ' + x + ' ) ', mc_selection))
-
-    arr = root2array(infile, 'B2dD0MuMu', selection=selstr)
-    print('Events after selection: ' + str(len(arr)))
+    logging.info('Events in blinded dataset: ' + str(len(arr)))
     array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
 
 @transform(blind_signalpeak, suffix('.root'), '.cut.root')
 def select(infile, outfile):
     from root_numpy import root2array, array2root
 
-    selstr = ' & '.join(map(lambda x: ' ( ' + x + ' ) ', selection))
+    trigger_lines = [
+        'B_L0MuonDecision_TOS == 1',
+        'B_Hlt1TrackAllL0Decision_TOS == 1',
+        'B_Hlt1TrackMuonDecision_TOS == 1',
+        'B_Hlt2Topo2BodyBBDTDecision_TOS == 1',
+        'B_Hlt2Topo3BodyBBDTDecision_TOS == 1',
+        'B_Hlt2Topo3BodyBBDTDecision_TOS == 1',
+        'B_Hlt2Topo4BodyBBDTDecision_TOS == 1',
+        'B_Hlt2TopoMu2BodyBBDTDecision_TOS == 1',
+        'B_Hlt2TopoMu3BodyBBDTDecision_TOS == 1',
+        'B_Hlt2TopoMu4BodyBBDTDecision_TOS == 1',
+        'Psi_Hlt2SingleMuonDecision_TOS == 1',
+        'B_Hlt2DiMuonDetachedDecision_TOS == 1',
+    ]
 
-    arr = root2array(infile, 'B2dD0MuMu', selection=selstr)
-    print('Events after selection: ' + str(len(arr)))
+    arr = root2array(infile, 'B2dD0MuMu', selection=prepare_sel(selection) + ' && (' + ' || '.join(trigger_lines) + ')')
+    logging.info('Events after selection: ' + str(len(arr)))
     array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
 
-@transform(select, suffix('.root'), '.misid.root')
-def add_mis_id(infile, outfile):
+#@transform(select, suffix('.root'), '.misid.root')
+def add_misid(infile, outfile):
     from root_numpy import root2array, array2root
     import numpy as np
     from itertools import product
@@ -176,8 +199,11 @@ def add_mis_id(infile, outfile):
 
     hypotheses = ['Pion', 'Kaon', 'Muon', 'Proton']
 
+    newnames = []
+    newfields = []
+
     for m_K, m_pi in product(hypotheses, hypotheses):
-        print 'Hypothesis: {}-{}'.format(m_K, m_pi)
+        logging.info('Hypothesis: {}-{}'.format(m_K, m_pi))
         K_pe  = np.sqrt(K_px**2  + K_py**2  + K_pz**2  + mass[m_K]**2)
         pi_pe = np.sqrt(pi_px**2 + pi_py**2 + pi_pz**2 + mass[m_pi]**2)
 
@@ -194,8 +220,10 @@ def add_mis_id(infile, outfile):
         D_m = np.sqrt(D_pe**2 - D_px**2 - D_py**2 - D_pz**2)
         B_m = np.sqrt(B_pe**2 - B_px**2 - B_py**2 - B_pz**2)
 
-        arr = append_field(arr, 'D_M_HYPO_K={},pi={}'.format(m_K, m_pi), D_m)
-        arr = append_field(arr, 'B_M_HYPO_K={},pi={}'.format(m_K, m_pi), B_m)
+        newfields.append(D_m)
+        newnames.append('D_M_HYPO_K={},pi={}'.format(m_K, m_pi))
+        newfields.append(B_m)
+        newnames.append('B_M_HYPO_K={},pi={}'.format(m_K, m_pi))
 
     # Hypothesis: Missing Kaon
     B_px = K_px + Psi_px
@@ -203,7 +231,8 @@ def add_mis_id(infile, outfile):
     B_pz = K_pz + Psi_pz
     B_pe = K_pe + Psi_pe
     B_m = np.sqrt(B_pe**2 - B_px**2 - B_py**2 - B_pz**2)
-    arr = append_field(arr, 'B_M_HYPO_K=Missing,pi=Pion', B_m)
+    newfields.append(B_m)
+    newnames.append('B_M_HYPO_K=Missing,pi=Pion')
 
     # Hypothesis: Missing Pion
     B_px = pi_px + Psi_px
@@ -211,11 +240,13 @@ def add_mis_id(infile, outfile):
     B_pz = pi_pz + Psi_pz
     B_pe = pi_pe + Psi_pe
     B_m = np.sqrt(B_pe**2 - B_px**2 - B_py**2 - B_pz**2)
-    arr = append_field(arr, 'B_M_HYPO_K=Kaon,pi=Missing', B_m)
+    newfields.append(B_m)
+    newnames.append('B_M_HYPO_K=Kaon,pi=Missing')
 
+    arr = append_fields(arr, newnames, newfields)
     array2root(arr, outfile, 'B2dD0MuMu', 'recreate')
 
-#@transform(select, suffix('.root'), add_inputs(select_mc), '.bdt.root')
+@transform(select, suffix('.root'), add_inputs(reduce_mc), '.classified.root')
 def classify(inputs, output):
     from root_numpy import root2array, array2root
     import numpy as np
@@ -226,8 +257,8 @@ def classify(inputs, output):
             'B_M > 5300'
     ]
 
-    selstr = ' & '.join(map(lambda x: ' ( ' + x + ' ) ', select_sidebands))
-    sidebands = root2array(fname, 'B2dD0MuMu', bdt_variables, selection=selstr, step=100)
+    mcname_new = mcname.replace('.root', '.classified.root')
+    sidebands = root2array(fname, 'B2dD0MuMu', bdt_variables, selection=prepare_sel(select_sidebands), step=100)
     mc = root2array(mcname, 'B2dD0MuMu', bdt_variables, step=100)
 
     from sklearn.tree import DecisionTreeClassifier
@@ -246,39 +277,35 @@ def classify(inputs, output):
             learning_rate=0.5
     )
 
-    print('Running x-validation...')
-    scores = cross_val_score(clf, X, y, cv=10, n_jobs=4)
-    from scipy.stats import sem
-    print('Scores: {} +/- {}'.format(np.mean(scores), sem(scores)))
+    #logging.info('Running x-validation...')
+    #scores = cross_val_score(clf, X, y, cv=10, n_jobs=4)
+    #from scipy.stats import sem
+    #logging.info('Scores: {} +/- {}'.format(np.mean(scores), sem(scores)))
     
     clf.fit(X, y)
 
+    logging.info("Variable importances:")
     for n, i in sorted(zip(sidebands.dtype.names, clf.feature_importances_), key=lambda x: -x[1]):
-        print n, i
+        logging.info("{} - {}".format(n, i))
 
     data_vars = root2array(fname, 'B2dD0MuMu', bdt_variables)
     data_vars = np.array(data_vars.tolist())
     data = root2array(fname, 'B2dD0MuMu')
 
     pred = clf.decision_function(data_vars)
-
-    data = append_field(data, 'classifier', pred)
+    data = append_fields(data, 'classifier', pred)
     array2root(data, output, 'B2dD0MuMu', 'recreate')
 
-def append_field(rec, name, arr, dtype=None):
-    import numpy as np
-    arr = np.asarray(arr)
-    if dtype is None:
-        dtype = arr.dtype
-    newdtype = np.dtype(rec.dtype.descr + [(name, dtype)])
-    newrec = np.empty(rec.shape, dtype=newdtype)
-    for field in rec.dtype.fields:
-        newrec[field] = rec[field]
-    newrec[name] = arr
-    return newrec
+    mc_vars = root2array(mcname, 'B2dD0MuMu', bdt_variables)
+    mc_vars = np.array(mc_vars.tolist())
+    mc = root2array(mcname, 'B2dD0MuMu')
+    pred = clf.decision_function(mc_vars)
+    mc = append_fields(mc, 'classifier', pred)
+    array2root(mc, mcname_new, 'B2dD0MuMu', 'recreate')
 
-@transform(add_mis_id, suffix('.root'), add_inputs(select_mc), '.pdf')
+@transform(classify, formatter(), add_inputs(reduce_mc), 'plots.pdf')
 def plot_vars(infile, outfile):
+    import numpy as np
     from root_numpy import root2array
     import matplotlib
     matplotlib.use('Agg')
@@ -289,71 +316,90 @@ def plot_vars(infile, outfile):
     sns.set_palette("deep", desat=.6)
 
     cuts = [
-        #'classifier > 0.1',
-        #'muplus_ProbNNmu > 0.80',
-        #'muminus_ProbNNmu > 0.80',
-        #'muplus_ProbNNpi < 0.40',
-        #'muminus_ProbNNpi < 0.40',
-        #'Psi_M < 3600 || Psi_M > 3800',
-        #'Psi_FDCHI2_ORIVX < 1',
-        #'Psi_FDCHI2_ORIVX > 0',
-        #'Psi_FD_ORIVX / Psi_FDCHI2_ORIVX < 0.1',
-        #'Kplus_ProbNNp < 0.10',
-        #'D~0_FD_ORIVX > 40',
-        #'D~0_FD_ORIVX < 60',
-        #'B_FD_OWNPV > 10',
-        #'B_FD_OWNPV < 60',
-        #'Kplus_ProbNNp > 0.0',
-        #'piminus_ProbNNp > 0.0',
-        #'D~0_M > 1845 && D~0_M < 1885',
-        #'Psi_M < 2950 || Psi_M > 3200',
-        #'Psi_M < 3500',
-        #'D~0_FD_ORIVX < 120',
-        #'B_M > 5150 && B_M < 5400',
-        #'Kplus_ProbNNpi < 0.2',
-        #'Kplus_ProbNNk > 0.6',
+        #'D~0_M > 1800 && D~0_M < 1920',
+        'B_M > 5150 && B_M < 5450',
+
+        'classifier > 0.30',
+
+        #'B_ISOLATION_BDT_Soft < 0',
+        #'B_ENDVERTEX_CHI2 < 10',
+        #'B_DIRA_OWNPV > 0.99996',
+        ##'Kplus_PT + piminus_PT > 1000',
+
+        #'D~0_DIRA_OWNPV > 0.9980',
+
+        #'muplus_ProbNNmu > 0.6',
+        #'muminus_ProbNNmu > 0.5',
+
+        #'Kplus_ProbNNpi < 0.05',
+        #'Kplus_ProbNNk > 0.8',
+        #'Kplus_ProbNNp < 0.02',
+
+        #'piminus_ProbNNpi > 0.9',
+        #'piminus_ProbNNp < 0.002',
+        #'piminus_ProbNNpi > 0.4',
+
+        #'Psi_FD_ORIVX < 1.5',
     ]
 
     data = infile[0]
-    mc = infile[1]
+    mc = infile[1].replace('.root', '.classified.root')
 
-    mc_new = mc.replace('.root', '.mc_misid.root')
-    add_mis_id(mc, mc_new)
+    arr = root2array(data, 'B2dD0MuMu', selection=prepare_sel(cuts))
+    total_mc = len(root2array(mc, 'B2dD0MuMu'))
+
+    arr_mc = root2array(mc, 'B2dD0MuMu', selection=prepare_sel(cuts))
+
+    factor = float(50) / total_mc
 
     with PdfPages(outfile) as pdf:
-        arr = root2array(data, 'B2dD0MuMu', selection=' && '.join(map(lambda x: '(' + x + ')', cuts)))
-        arr_mc = root2array(mc_new, 'B2dD0MuMu', selection=' && '.join(map(lambda x: '(' + x + ')', cuts)))
-
         for vname in arr.dtype.names:
-            print('Plotting ' + vname)
-            plt.hist(arr[vname], histtype='stepfilled', bins=100, alpha=0.7, normed=True)
-            plt.hist(arr_mc[vname], histtype='stepfilled', bins=100, alpha=0.7, normed=True)
+            logging.info('Plotting ' + vname)
+            n, bins, _ = plt.hist(arr[vname], histtype='stepfilled', bins=100, alpha=0.8)
+            if vname in arr_mc.dtype.names:
+                n_mc, edges = np.histogram(arr_mc[vname], bins)
+                binned_hist(plt.gca(), factor * n_mc, edges, histtype='stepfilled', alpha=0.8)
+
+                #plt.hist(arr_mc[vname], histtype='stepfilled', bins=bins, alpha=0.8, normed=True)
             #plt.yscale('log')
+            if 'B_M' in vname:
+                plt.axvline(5279, color='b', ls='--')
             plt.xlabel(vname)
+            plt.ylim(0, max(n) * 1.05)
             pdf.savefig()
             plt.clf()
 
+        logging.info('Plotting m_B vs. q^2')
         jp = sns.jointplot(arr['B_M'], arr['Psi_M'], kind='hex', joint_kws={'norm': LogNorm()})
         jp.set_axis_labels('$m_{B^0}$', '$q^2_{\\mu\\mu}$')
         plt.tight_layout()
         pdf.savefig()
         plt.clf()
 
+        logging.info('Plotting m_D vs. q^2')
         jp = sns.jointplot(arr['D~0_M'], arr['Psi_M'], kind='hex', joint_kws={'norm': LogNorm()})
         jp.set_axis_labels('$m_{\\bar{D}^0}$', '$q^2_{\\mu\\mu}$')
         plt.tight_layout()
         pdf.savefig()
         plt.clf()
 
+def prepare_sel(selections):
+    return ' && '.join(map(lambda x: '(' + x + ')', selections))
+
+def calc_tau(arr):
+    from scipy.constants import c
+    arr = arr['B_FD_OWNPV'] * arr['B_M'] / (arr['B_P'] * c * 10**3) * 10**12
+    print(arr)
+    print(arr.dtype)
+    return arr
+
+def binned_hist(ax, data, binedges, *args, **kwargs):
+    #The dataset values are the bin centres
+    x = (binedges[1:] + binedges[:-1]) / 2.0
+    #The weights are the y-values of the input binned data
+    weights = data
+    return ax.hist(x, bins=binedges, weights=weights, *args, **kwargs)
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1:
-        pipeline_run(forcedtorun_tasks=sys.argv[1:])
-    else:
-        pipeline_run()
-
-    pipeline_printout_graph("flow.pdf", "pdf",
-                            forcedtorun_tasks = [reduce],
-                            no_key_legend = True)
-
+    pipeline_run(forcedtorun_tasks=sys.argv[1:])

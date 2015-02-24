a60c4ed995f9baec15c53e923a180af187f87117
Igor Babuschkin <igor@babuschk.in>
2015-02-24 17:04:27 +0100

    Add Gauss options for testing new decfiles

---
+++ mc/options/Bu_Ds+mumu=DecProdCut/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gaudirun.py options.py $GAUSSOPTS/Gauss-2012.py $GAUSSOPTS/GenStandAlone.py $DECFILESROOT/options/12275016.py $LBPYTHIAROOT/options/Pythia.py
+
---
+++ mc/options/Bu_Dst+mumu=DecProdCut/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gaudirun.py options.py $GAUSSOPTS/Gauss-2012.py $GAUSSOPTS/GenStandAlone.py $DECFILESROOT/options/12175022.py $LBPYTHIAROOT/options/Pythia.py
+

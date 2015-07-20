39d62858df6665cb900e22997bd2e0b7a567095e
Igor Babuschkin <igor@babuschk.in>
2015-07-20 14:43:06 +0200

    Add better and improved option file

--- lhcb/data/options.py
+++ lhcb/data/options.py
@@ -1,39 +1,138 @@
 
 
 from Gaudi.Configuration import *
-from Configurables import  DaVinci
-from DiLeptonTuple.DiLeptonTuple import addTuple
+from Configurables import  DaVinci, DecayTreeTuple, TupleToolTISTOS
+from DiLeptonTuple.AllTriggerLines import muonLines
 
-def create_options(decay, outname, year, name="B2XMuMu_Line", mc=False):
+def create_tuples(mc):
 
     if mc:
-        head = "/Event/AllStreams/Phys"
-        addendum = "MC"
+        kind='MC'
+        path = "/Event/AllStreams/Phys/B2XMuMu_Line/Particles"
     else:
-        head = "/Event/Dimuon/Phys"
-        addendum = "DST"
+        kind='DATA'
+        path = "/Event/Dimuon/Phys/B2XMuMu_Line/Particles"
 
-    DaVinci().appendToMainSequence(addTuple(
-        name=name,
-        head=head,
-        decay=decay,
-        dtf=True,
-        resonant=False,
-        addendum=addendum,
-        verbose=["Pid", "iso", "Kinematic"]
-    ))
+    mesons = [
+        {
+            'particle': 'D~0',
+            'decay': 'B_Dmumu',
+        },
+        {
+            'particle': 'K*(892)',
+            'decay': 'B_JpsiKst',
+        }]
+
+    sequence = GaudiSequencer('DataSequence')
+
+    for meson in mesons:
+
+        data = DecayTreeTuple('{}_{}'.format(kind, meson['decay']))
+        data.TupleName = 'default'
+        
+        geometry = data.addTupleTool('TupleToolGeometry')
+        geometry.FillMultiPV = True
+
+        triggerLines = muonLines()
+        tistos = TupleToolTISTOS(TriggerList=triggerLines,
+                                 VerboseHlt1=True,
+                                 VerboseHlt2=True,
+                                 VerboseL0=True)
+        
+        data.Branches = {
+                'B' :       '[B0 ->  (J/psi(1S) ->  mu+  mu-)  ({} ->  K+  pi-) ]CC'.format(meson['particle']),
+                'mumu' :    '[B0 -> ^(J/psi(1S) ->  mu+  mu-)  ({} ->  K+  pi-) ]CC'.format(meson['particle']),
+                'AntiD0':   '[B0 ->  (J/psi(1S) ->  mu+  mu-) ^({} ->  K+  pi-) ]CC'.format(meson['particle']),
+                'Kplus':    '[B0 ->  (J/psi(1S) ->  mu+  mu-)  ({} -> ^K+  pi-) ]CC'.format(meson['particle']),
+                'piminus':  '[B0 ->  (J/psi(1S) ->  mu+  mu-)  ({} ->  K+ ^pi-) ]CC'.format(meson['particle']),
+                'muplus':   '[B0 ->  (J/psi(1S) -> ^mu+  mu-)  ({} ->  K+  pi-) ]CC'.format(meson['particle']),
+                'muminus':  '[B0 ->  (J/psi(1S) ->  mu+ ^mu-)  ({} ->  K+  pi-) ]CC'.format(meson['particle']),
+        }
+
+        mumu = data.addTupleTool('TupleToolDecay/mumu')
+        mumu.ToolList += ['TupleToolTISTOS']
+
+        B = data.addTupleTool('TupleToolDecay/B')
+
+        p2vv = B.addTupleTool('TupleToolP2VV')
+        p2vv.Calculator = 'Bd2KstarMuMuAngleCalculator'
+
+        angles = data.addTupleTool('TupleToolAngles')
+        angles.WRTMother = False
+
+        constrainJpsi = B.addTupleTool('TupleToolDecayTreeFitter/ConstrainJpsi')
+        constrainJpsi.Verbose = True
+        constrainJpsi.constrainToOriginVertex = True
+        constrainJpsi.daughtersToConstrain + ['J/psi(1S)']
+
+        constrainMeson = B.addTupleTool('TupleToolDecayTreeFitter/ConstrainMeson')
+        constrainMeson.Verbose = True
+        constrainJpsi.constrainToOriginVertex = True
+        constrainJpsi.daughtersToConstrain + ['D~0']
+
+        recoStats = data.addTupleTool('TupleToolRecoStats')
+        eventInfo = data.addTupleTool('TupleToolEventInfo')
+        kinematic = data.addTupleTool('TupleToolKinematic')
+        primares = data.addTupleTool('TupleToolPrimaries')
+        trackInfo = data.addTupleTool('TupleToolTrackInfo')
+        pid = data.addTupleTool('TupleToolPid')
+        pid.Verbose = True
+        data.addTupleTool('TupleToolL0Calo')
+
+        from configIso import configIso
+        configIso()
+
+        from Configurables import TupleToolApplyIsolation
+        data.B.addTool(TupleToolApplyIsolation, name="TupleToolApplyIsolationHard")
+        data.B.TupleToolApplyIsolationHard.OutputSuffix="_Hard"
+        data.B.TupleToolApplyIsolationHard.WeightsFile="weightsHard.xml"
+        data.B.ToolList+=["TupleToolApplyIsolation/TupleToolApplyIsolationHard"]
+        data.B.addTool(TupleToolApplyIsolation, name="TupleToolApplyIsolationSoft")
+        data.B.TupleToolApplyIsolationSoft.OutputSuffix="_Soft"
+        data.B.TupleToolApplyIsolationSoft.WeightsFile="weightsSoft.xml"
+        data.B.ToolList+=["TupleToolApplyIsolation/TupleToolApplyIsolationSoft"]  
+        vtxiso = data.B.addTupleTool("TupleToolVtxIsoln")
+        data.B.TupleToolApplyIsolationHard.OutputLevel = 3 
+        data.B.TupleToolApplyIsolationSoft.OutputLevel = 3 
+
+        data.addTupleTool('TupleToolPropertime')
+        tts = data.addTupleTool('TupleToolStripping')
+        data.addTupleTool('TupleToolPropertime')
+
+        if mc:
+            mcTruth = data.addTupleTool('TupleToolMCTruth')
+            mcTruth.addTupleTool('MCTupleToolHierarchy')
+            data.addTupleTool('TupleToolMCBackgroundInfo')
+
+        sequence.Members.append(data)
+
+    return [sequence]
+
+def create_options(year, magnet, mc):
+    DaVinci().appendToMainSequence(create_tuples(mc))
 
     DaVinci().EvtMax = -1
-    DaVinci().TupleFile = outname
-    DaVinci().Lumi = True
+    DaVinci().TupleFile = 'out.root'
+
     DaVinci().DataType = str(year)
 
+    if mc and magnet == 'up':
+        DaVinci.Simulation() = True
+        DaVinci().CondDBtag = "sim-20130522-1-vc-mu100"
+        DaVinci().DDDBtag = "dddb-20130929-1"
+    elif mc and magnet == 'down':
+        DaVinci().Simulation = True
+        DaVinci().CondDBtag = "sim-20130522-1-vc-md100"
+        DaVinci().DDDBtag = "dddb-20130929-1"
     if year == 2012:
+        DaVinci().Lumi = True
         DaVinci().CondDBtag = "cond-20120831"
         DaVinci().DDDBtag = "dddb-20120831"
     elif year == 2011:
+        DaVinci().Lumi = True
         DaVinci().CondDBtag = "cond-20130114"
         DaVinci().DDDBtag = "dddb-20130111"
     else:
         raise ValueError("unknown year: {}".format(year))
+
 
--- lhcb/data/run.py
+++ lhcb/data/run.py
@@ -1,10 +1,6 @@
 
 decays = {
-    #'Bd_D0mumu':   '[B0   -> ^(D~0 -> ^K+ ^pi-) ^(J/psi(1S) -> ^mu+ ^mu-) ]CC',
-    #'Bc_Ds+mumu':  '[B_c+ -> ^(D_s+ -> ^K+ ^K- ^pi+) ^(J/psi(1S) -> ^mu+ ^mu-)]CC',
-    #'Bu_Ds+mumu':  '[B+   -> ^(D_s+ -> ^K+ ^K- ^pi+) ^(J/psi(1S) -> ^mu+ ^mu-]CC',
-    #'Bu_Dst+mumu': '[B+   -> ^(D*(2010)+ -> ^(D0 -> ^K- ^pi+) ^pi+) ^(J/psi(1S) -> ^mu+ ^mu-)]CC',
-    'Bd_KstJpsi':   '[B0   -> ^(K*(892)0 -> ^K+ ^pi-) ^(J/psi(1S) -> ^mu+ ^mu-) ]CC',
+    'Bd_D0mumu':   '[B0   -> ^(D~0 -> ^K+ ^pi-) ^(J/psi(1S) -> ^mu+ ^mu-) ]CC',
 }
 
 stream = "/LHCb/Collision{year}/Beam{energy}GeV-VeloClosed-{polarity}/Real Data/Reco14/Stripping{stripping}/90000000/DIMUON.DST"
@@ -22,8 +18,7 @@ for decay in decays.keys():
             filepath = stream.format(year=str(year)[-2:], energy=energy, polarity=polarity, stripping=stripping)
             data = BKQuery(filepath, dqflag=['OK']).getDataset()
 
-            name = 'DATA_' + decay + "_" + str(year) + "_" + polarity
-            outname = decay + '.root'
+            name = 'DATA_B_Dmumu_' + str(year) + '_' + polarity
             j = Job(name=name)
             print(j.name)
             j.application = DaVinci()
@@ -31,9 +26,9 @@ for decay in decays.keys():
             j.inputdata = data
             j.application.make()
             j.application.optsfile = './options.py'
-            j.application.extraopts = 'create_options(decay="{decay}", year={year}, outname="{outname}")'.format(decay=decays[decay], year=year, outname=outname)
+            j.application.extraopts = 'create_options(year={year}, )'.format(year=repr(year))
             j.inputsandbox = ['./weightsHard.xml', './weightsSoft.xml']
             j.backend = Dirac()
             j.splitter = SplitByFiles(filesPerJob=5)
-            j.merger = RootMerger(files=[outname])
+            j.merger = RootMerger(files=['out.root'])
 
